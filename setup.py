6ec404c64b668d31fe9edd9faa1f95280ac9d152
MvMukesh <mukeshmanral777@gmail.com>
2025-01-04 14:48:46 +0530

    setup code updated

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 import setuptools
 
 
-with open("README.md","r", encoding="utf-8") as f:
+with open("README.md","r",encoding="utf-8") as f:
     long_description = f.read()
 
 __version__ = "0.1.0"
-REPO_NAME = 'linklns'
-SRC_REPO = 'linklns'
+REPO_NAME = "linklns"
+SRC_REPO = "linklns"
 AUTHOR_USER_NAME = "MvMukesh"
 AUTHOR_EMAIL = "mukeshmanral777@gmail.com"
+
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A Python tool to link media into Notebook",
+    description="Python Package link_lens bridges missing link between your digital content and your NoteBook",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={"Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"},
     package_dir={"":"src"},
-    packages=setuptools.find_packages("src")
-    )
+    packages=setuptools.find_packages(where="src"))
