121096080add9ce3e6adff75fdbe08fc31db136f
MvMukesh <mukeshmanral777@gmail.com>
2025-01-04 13:02:04 +0530

    setup code added

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+with open("README.md","r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.1.0"
+REPO_NAME = 'linklns'
+SRC_REPO = 'linklns'
+AUTHOR_USER_NAME = "MvMukesh"
+AUTHOR_EMAIL = "mukeshmanral777@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python tool to link media into Notebook",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={"Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"},
+    package_dir={"":"src"},
+    packages=setuptools.find_packages("src")
+    )
