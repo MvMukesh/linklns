ade6c3afba0524cb0438d2df96367606b510425a
MvMukesh <mukeshmanral777@gmail.com>
2025-01-04 14:51:59 +0530

    code update 1

--- init_setup.sh
+++ init_setup.sh
@@ -1,10 +1,10 @@
-echo [$(date)]: "START"
-echo [$(date)]: "Create conda env using Python 3.8"
-conda create --prefix ./env python=3.8 -y
-
-echo [$(date)]: "Activate conda env"
+echo [$(date)]:"START"
+echo [$(date)]:"Creating conda env using Python 3.9"
+conda create --prefix ./env python=3.9 -y
+``
+echo [$(date)]:"Activating env"
 source activate ./env
 
-echo [$(date)]: "Install dev requirements"
+echo [$(date)]:"Installing dev requirements"
 pip install -r requirements_dev.txt
-echo [$(date)]: "END"
+echo [$(date)]:"END"
